5a7374584e571aef341e6db910cd60b34cfbc8aa
Mukund Solanki <mukundsolanki665@gmail.com>
2023-07-07 00:02:13 +0530

    Completed the help component

---
+++ src/components/Help.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Help() {
+    return (
+        <div >
+            <p className='help'>
+                <span className='commands'>about</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--If you want to stalk me.
+            </p>
+
+            <p className='help'>
+                <span className='commands'>education</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--My educational background.
+            </p>
+
+            <p className='help'>
+                <span className='commands'>skils</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--Some pretty decent skills.
+            </p>
+
+            <p className='help'>
+                <span className='commands'>socials</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--Lets connect!
+            </p>
+
+            <p className='help'>
+                <span className='commands'>clear</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--If you messed up playing with the terminal:)
+            </p>
+        </div>
+    )
+}
