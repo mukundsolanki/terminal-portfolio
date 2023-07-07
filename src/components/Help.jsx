7d1d7fd3ff1d13649376fb707c91f3114341c280
Mukund Solanki <mukundsolanki665@gmail.com>
2023-07-07 18:06:49 +0530

    Updated the help component with some set of new commands and worked on Social Component

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -4,23 +4,35 @@ export default function Help() {
     return (
         <div >
             <p className='help'>
-                <span className='commands'>about</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--If you want to stalk me.
+                <span className='commands'>about</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--  If you want to stalk me.
             </p>
 
             <p className='help'>
-                <span className='commands'>education</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--My educational background.
+                <span className='commands'>skills</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--  Some pretty decent skills.
             </p>
 
             <p className='help'>
-                <span className='commands'>skils</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--Some pretty decent skills.
+                <span className='commands'>projects</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--  My Projects.
             </p>
 
             <p className='help'>
-                <span className='commands'>socials</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--Lets connect!
+                <span className='commands'>socials</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--  Lets connect!
             </p>
 
             <p className='help'>
-                <span className='commands'>clear</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--If you messed up playing with the terminal:)
+                <span className='commands'>clear</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-- If you messed up playing with the terminal:)
+            </p>
+
+            <p className='help'>
+                <span className='commands'>github</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-- Head over to my github
+            </p>
+
+            <p className='help'>
+                <span className='commands'>source</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-- If you want to make something like this...
+            </p>
+
+            <p className='help'>
+                <span className='commands'>GUI</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-- Switch to GUI.
             </p>
         </div>
     )
---
+++ src/components/Socials.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Socials() {
+  return (
+    <div >
+      <p className='help'>
+        <span className='commands'>1. linkedin</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.linkedin.com/in/mukund-solanki-10a558250/" rel="noreferrer" target='_blank'>https://linkedin.com/in/mukund-solanki-10a558250/</a>
+      </p>
+
+      <p className='help'>
+        <span className='commands'>2. twiter</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://twitter.com/Mukund_solanki_" rel="noreferrer" target='_blank'>https://twitter.com/Mukund_solanki_</a>
+      </p>
+
+      <p className='help'>
+        <span className='commands'>3. github</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/mukundsolanki" rel="noreferrer" target='_blank'>https://github.com/mukundsolanki</a>
+      </p>
+
+      <p>For a list of available commands, type '<span className='commands'>help</span>'.</p>
+    </div>
+  )
+}
